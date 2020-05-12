af042d7b2df5dc1ce466e8f3c5f3260bbf00339f
Inga Chubareva <ingach23@gmail.com>
2020-05-12 14:04:01 +0300

    Updates

---
+++ precache-manifest.0a4dafcb06e4dcb2b2e2c0dc088880b3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c27ac4e448c722c0de149cd0f26380",
+    "url": "/React-Project/index.html"
+  },
+  {
+    "revision": "b4cc6852dc91beaf0cc3",
+    "url": "/React-Project/static/css/3.3962b0af.chunk.css"
+  },
+  {
+    "revision": "4c460efe12e724780425",
+    "url": "/React-Project/static/css/4.5c5e084c.chunk.css"
+  },
+  {
+    "revision": "45d9ab6967b23137d184",
+    "url": "/React-Project/static/css/main.3724e9d2.chunk.css"
+  },
+  {
+    "revision": "c1b0554b80fb8286f603",
+    "url": "/React-Project/static/js/2.b233cf55.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/React-Project/static/js/2.b233cf55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4cc6852dc91beaf0cc3",
+    "url": "/React-Project/static/js/3.295372e6.chunk.js"
+  },
+  {
+    "revision": "4c460efe12e724780425",
+    "url": "/React-Project/static/js/4.2ecaf5a9.chunk.js"
+  },
+  {
+    "revision": "45d9ab6967b23137d184",
+    "url": "/React-Project/static/js/main.2da0a60c.chunk.js"
+  },
+  {
+    "revision": "426709dbcd19d3cebbcd",
+    "url": "/React-Project/static/js/runtime-main.07f5476b.js"
+  },
+  {
+    "revision": "6c2d4f522b3978cd51640f3d8fc2a2fb",
+    "url": "/React-Project/static/media/2168070.6c2d4f52.png"
+  },
+  {
+    "revision": "223f86f01826af5f2636fc9944917113",
+    "url": "/React-Project/static/media/Spinner-1s-84px.223f86f0.svg"
+  },
+  {
+    "revision": "06fd3ccfb4457544a5e6996cfdedb8a7",
+    "url": "/React-Project/static/media/avatar.06fd3ccf.png"
+  },
+  {
+    "revision": "89a3edfa3a5221b5854e427330eef735",
+    "url": "/React-Project/static/media/chat.89a3edfa.png"
+  },
+  {
+    "revision": "9bdf567aab02e3de6863bdcf682a9f89",
+    "url": "/React-Project/static/media/gear.9bdf567a.png"
+  },
+  {
+    "revision": "68d93468f36147bc154014ae4177e151",
+    "url": "/React-Project/static/media/man.68d93468.png"
+  }
+]);
